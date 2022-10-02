737838f55493bd47b9b3af45f9bf3215da7b709b
thj0309 <thj0309@hotmail.com>
2022-10-02 15:46:24 +0900

    init commit

--- src/main/java/com/frontend/controller/HelloworldController.java
+++ src/main/java/com/frontend/controller/HelloworldController.java
@@ -1,2 +1,13 @@
-package com.frontend.controller;public class HelloworldController {
+package com.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloworldController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
