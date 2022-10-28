cf5cf4775cb024b71c030997ee7a948c01d026b8
thj0309 <thj0309@cj.net>
2022-10-28 14:24:15 +0900

    grafana, prometheus 반영

--- src/main/java/com/frontend/controller/CatalogController.java
+++ src/main/java/com/frontend/controller/CatalogController.java
@@ -1,10 +1,9 @@
 package com.frontend.controller;
 
-import com.frontend.feign.FeignUserController;
+import com.frontend.feign.FeignCatalogController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -12,13 +11,11 @@ import java.util.Map;
 @Controller
 @RequestMapping(value="/catalog")
 public class CatalogController {
-    private final FeignUserController feignUserController;
+    private final FeignCatalogController feignCatalogController;
 
-    public CatalogController(FeignUserController feignUserController) {
-        this.feignUserController = feignUserController;
+    public CatalogController(FeignCatalogController feignCatalogController) {
+        this.feignCatalogController = feignCatalogController;
     }
-    //@Value("${page.user-url}")
-	//private String userUrl;
 
     @GetMapping("/page")
     public String getPage(Model model){
@@ -29,18 +26,19 @@ public class CatalogController {
         return "catalog";
     }
 
-    @GetMapping("/test")
-    public String getUserPage(){
+    @GetMapping("/catalogs")
+    @ResponseBody
+    public List<Map<String, String>> getCatalogList(){
         // 웹어플리케이션에서 '/hello' 라고 요청이 들어오면 해당 메서드를 호출한다.
         // 스프링 MVC가 내부에서 @Controller를 찾고, 거기에 있는 @GetMapping의 정보를 다 등록해둡니다.
         // 그래서 URL에 동일한 경로가 있으면 호출해줍니다.
-        return "helloworld!!";
+        return feignCatalogController.getCatalogListForFeign();
     }
 
-    @GetMapping("/users")
-    public String getUsersList(Model model){
-        List<Map<String, String>> result = feignUserController.getUserForFeign();
-        model.addAttribute("result", result); // 키값과 밸류값을 지정해서 model에 담아서 view에 넘긴다.
-        return "userlist";
+    @PostMapping("/catalogs")
+    @ResponseBody
+    public List<Map<String, String>> saveCatalog(@RequestBody Map<String, String> input ){
+        feignCatalogController.saveCatalogForFeign(input);
+        return getCatalogList();
     }
 }
---
+++ src/main/java/com/frontend/feign/FeignCatalogController.java
@@ -0,0 +1,17 @@
+package com.frontend.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Map;
+
+@FeignClient(name="2022pmds-backend-catalog")
+public interface FeignCatalogController {
+    @GetMapping("/catalog/feign/list")
+    List<Map<String, String>> getCatalogListForFeign();
+
+    @PostMapping("/catalog/feign/list")
+    List<Map<String, String>> saveCatalogForFeign(Map<String, String> input);
+}
