ae60a0b157015a0a21c9f28023821a2f69bac59f
thj0309 <thj0309@cj.net>
2022-11-22 16:30:31 +0900

    최종 수정

--- src/main/java/com/frontend/controller/OrderController.java
+++ src/main/java/com/frontend/controller/OrderController.java
@@ -1,14 +1,16 @@
 package com.frontend.controller;
 
+import com.frontend.feign.FeignCatalogController;
 import com.frontend.feign.FeignOrderController;
+import com.frontend.feign.FeignUserController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
@@ -17,9 +19,14 @@ import java.util.Map;
 @RequestMapping(value="/order")
 public class OrderController {
     private final FeignOrderController feignOrderController;
+    private final FeignUserController feignUserController;
 
-    public OrderController(FeignOrderController feignOrderController) {
+    private final FeignCatalogController feignCatalogController;
+
+    public OrderController(FeignOrderController feignOrderController, FeignUserController feignUserController, FeignCatalogController feignCatalogController) {
         this.feignOrderController = feignOrderController;
+        this.feignUserController = feignUserController;
+        this.feignCatalogController = feignCatalogController;
     }
     //@Value("${page.user-url}")
 	//private String userUrl;
@@ -30,6 +37,10 @@ public class OrderController {
         // 스프링 MVC가 내부에서 @Controller를 찾고, 거기에 있는 @GetMapping의 정보를 다 등록해둡니다.
         // 그래서 URL에 동일한 경로가 있으면 호출해줍니다.
         //model.addAttribute("userUrl", userUrl); // 키값과 밸류값을 지정해서 model에 담아서 view에 넘긴다.
+
+        // 사용자 목록
+        model.addAttribute("userList", feignUserController.getUserForFeign()); // 키값과 밸류값을 지정해서 model에 담아서 view에 넘긴다.
+        model.addAttribute("catalogList", feignCatalogController.getCatalogListForFeign()); // 키값과 밸류값을 지정해서 model에 담아서 view에 넘긴다.
         return "order";
     }
 
@@ -46,5 +57,14 @@ public class OrderController {
     public List<Map<String, String>> getOrderListByUser(@PathVariable String userId){
 
         return feignOrderController.getOrder(userId);
+    }
+
+    @PostMapping("/orders/{userId}")
+    @ResponseBody
+    public void postOrderByUser(@PathVariable String userId
+                                                    , @RequestBody Map<String, String> input
+    ){
+        userId = URLDecoder.decode(userId, StandardCharsets.UTF_8);
+        feignOrderController.postOrder(userId, input);
     }
 }
--- src/main/java/com/frontend/feign/FeignOrderController.java
+++ src/main/java/com/frontend/feign/FeignOrderController.java
@@ -3,6 +3,7 @@ package com.frontend.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Map;
@@ -11,4 +12,7 @@ import java.util.Map;
 public interface FeignOrderController {
     @GetMapping("/order/feign/{userId}/list")
     List<Map<String, String>> getOrder(@PathVariable String userId);
+
+    @PostMapping("/order/feign/{userId}/orders")
+    void postOrder(@PathVariable String userId, Map<String, String> input);
 }
